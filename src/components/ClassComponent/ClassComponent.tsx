825e74372c6bb9e5cf1f402af77c05ea078ac9ec
Valdinei Teodoro <83255004+valdineiteodoro@users.noreply.github.com>
2022-11-17 15:20:13 -0400

    add

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -3,10 +3,21 @@ import React from "react"
 
 
 class ClassComponent extends React.Component {
+    constructor(props:any){
+        super(props)
+        console.log("construtor reached")
+    }
 state ={
     name: "mundo"
 }
+
+
+componentDidMount(){
+     console.log("did mount reached")
+    
+}
 render(){
+    console.log("redner reached")
     return <div>
     <p>name: {this.state.name}</p>
         <button onClick={()=>{this.setState({name:"valdine"}
