1aa2fbd2534fe9dae40c92f64ef3aa4e7441b5d7
Valdinei Teodoro <83255004+valdineiteodoro@users.noreply.github.com>
2022-11-18 18:15:13 -0400

    add

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,30 +1,64 @@
 import React from 'react'
+import organizeData from '../../util/organizeDataForm'
 import './Table.scss'
-import Products from './Table.mockdata'
 
-const headers = [
-  { key: 'name', value: 'Product' },
-  { key: 'price', value: 'Price' },
-  { key: 'actions', value: 'Actions' },
-  { key: 'stock', value: 'Available Stock' },
-]
+ export declare interface TableHeader {
+  key: string
+  value: string
+  right?: boolean
+}
 
-const Table = () => {
+declare interface TableProps{
+  header:TableHeader[]
+    data: any[]
+
+    enableActions?: boolean
+
+    onDelete?: (intem : any ) => void
+    onDetail?: (intem : any ) => void
+    onEdit?: (intem : any ) => void
+  
+ }
+
+
+
+const Table :React.FC<TableProps> = (props) => {
+  const [organizedData, indexedHeaders] = organizeData(props.data, props.header)
   return <table className="AppTable">
     <thead>
       <tr>
         {
-          headers.map(header => <th key={header.key}>{header.value}</th>)
+          props.header.map(header =>
+            <th
+              className={header.right ? 'right' : ''}
+              key={header.key}
+            >
+              {header.value}
+            </th>
+          )
         }
       </tr>
     </thead>
     <tbody>
       {
-        Products.map(product => <tr>
-          <td>{ product.name }</td>
-          <td>${product.price}</td>
-          <td className="right">{product.stock}</td>
-        </tr>)
+        organizedData.map((row, i) => {
+          return <tr key={i}>
+            {
+              Object
+                .keys(row)
+                .map((item, i) =>
+                  item !== '$original'
+                    ? <td
+                        key={row.$original.id + i}
+                        className={indexedHeaders[item].right ? 'right' : ''}
+                      >
+                        { row[item] }
+                      </td>
+                    : null
+                )
+            }
+          </tr>
+        })
       }
     </tbody>
   </table>
