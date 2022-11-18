d674ff03dbb70f18607a0300811fa7a6a58bed6e
Valdinei Teodoro <83255004+valdineiteodoro@users.noreply.github.com>
2022-11-18 11:37:15 -0400

    add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../shared/Button';
-import Input from '../../shared/Input';
 import Container from '../../shared/Container';
-
-
-function TestComponent() {
-  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
-}
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Button
-          onClick={() => window.alert('UIIIU')}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
-        <Input 
-        label="street"/>
+      
       </Container>
     </div>
 
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+ import "./Table.scss"
+
+const Table = () =>{
+    return <table className='AppTable'>
+
+    </table>
+} 
+
+export default Table
