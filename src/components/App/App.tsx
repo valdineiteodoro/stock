032672c2e625266881eec81f30ed94c966229fb9
Valdinei Teodoro <83255004+valdineiteodoro@users.noreply.github.com>
2022-11-17 21:46:09 -0400

    add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../Button';
+import Button from '../../shared/Button';
+import Container from '../../shared/Container';
 
 function TestComponent () {
   return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
@@ -12,15 +13,15 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
 
-      <div className="Container">
+      <Container>
         <Button
           onClick={() => window.alert('UIIIU')}
-          appendIcon={<TestComponent />}
-        >
+          appendIcon={<TestComponent />}>
           Alert
         </Button>
+        </Container>
       </div>
-    </div>
+    
   );
 }
 
