c8fab295791e6101db781cb468ed1543fb3f9101
Valdinei Teodoro <83255004+valdineiteodoro@users.noreply.github.com>
2022-11-23 17:50:03 -0400

    add

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import './index.css';
+import http from './util/http'
 
+http.get("/posts")
+http.get("/posts/1")
+http.get("https://viacep.com.br/ws/01001000/json/")
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
